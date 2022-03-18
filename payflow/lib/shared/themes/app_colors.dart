1accf6ef6e442b8c92cf0833d298a262474cc895
João Lucas Pereira Rafael <joaosombrio@rede.ulbra.br>
2022-03-17 22:37:42 -0300

    config

---
+++ payflow/lib/shared/themes/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static final primary =Color(0xFFFF941A);
+  static final grey =Color(0XFF585666);
+  static final delete =Color(0XFFE83F5B);
+  static final heading =Color(0XFF585666);
+  static final body =Color(0XFF706E7A);
+  static final stroke =Color(0XFFE3E3E6);
+  static final shape =Color(0XFFFAFAFC);
+  static final background =Color(0XFFFFFFFF);
+}
